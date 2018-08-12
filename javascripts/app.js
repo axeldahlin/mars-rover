67d4f1e35352006acd556759ced6dc8339e1e8c7
Axel <axdahlin@gmail.com>
2018-08-12 08:30:35 +0200

    add clones that move on their own

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,17 +1,49 @@
-// Rover Object Goes Here
+//Coded by Axel Dahlin
+// to controll any rover from the console: validateCommand(command, marsRover)
+//Commands:
+// f - forward
+// b- backward
+// l- turn left
+// r - turn right
+
+// marsRover Objects Goes Here
 // ======================
 let rover = {
   direction: "N",
   x: 0,
   y: 0,
-  travelLog: []
+  travelLog: [],
+  letterOnMap: 'R'
 };
 
+let roverCloneOne = {
+  direction: "N",
+  x: 9,
+  y: 0,
+  travelLog: [],
+  letterOnMap: 'C'
+};
+
+let roverCloneTwo = {
+  direction: "N",
+  x: 9,
+  y: 9,
+  travelLog: [],
+  letterOnMap: 'C'
+};
+
+let roverCloneThree = {
+  direction: "N",
+  x: 0,
+  y: 9,
+  travelLog: [],
+  letterOnMap: 'C'
+};
 
 // Map Goes Here
 // ======================
 const map = [
-  ['R', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+  ['R', '_', '_', '_', '_', '_', '_', '_', '_', 'C'],
   ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
   ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
   ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
@@ -20,76 +52,67 @@ const map = [
   ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
   ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
   ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-  ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
+  ['C', '_', '_', '_', '_', '_', '_', '_', '_', 'C']
 ];
 
 function generateObstacles() {
   for (let i = 0; i < 5; i++) {
-  const randomNum1 = Math.floor(Math.random() * 10);
-  const randomNum2 = Math.floor(Math.random() * 10);
+  const randomNum1 = Math.floor(Math.random() * 8) + 1;
+  const randomNum2 = Math.floor(Math.random() * 8) + 1;
   map[randomNum1][randomNum2] = 'X';
   }
 }
 
 generateObstacles();
 
-
-
-
 // ======================
-function turnLeft(rover){
+function turnLeft(marsRover){
   console.log("turnLeft was called!");
-  switch(rover.direction) {
+  switch(marsRover.direction) {
     case 'N':
-      rover.direction = 'W'
+    marsRover.direction = 'W'
       break;
     case 'E':
-      rover.direction = 'N'
+    marsRover.direction = 'N'
       break;
     case 'S':
-      rover.direction = 'E'
+    marsRover.direction = 'E'
       break;
     case 'W':
-      rover.direction = 'S';
+    marsRover.direction = 'S';
       break;
   }
-  console.log(`Direction: ${rover.direction}`);
+  console.log(`Direction: ${marsRover.direction}`);
 }
 
-function turnRight(rover){
+function turnRight(marsRover){
   console.log("turnRight was called!");
-  switch(rover.direction) {
+  switch(marsRover.direction) {
     case 'N':
-      rover.direction = 'E'
+    marsRover.direction = 'E'
       break;
     case 'E':
-      rover.direction = 'S'
+    marsRover.direction = 'S'
       break;
     case 'S':
-      rover.direction = 'W'
+    marsRover.direction = 'W'
       break;
     case 'W':
-      rover.direction = 'N';
+    marsRover.direction = 'N';
       break;
   }
-  console.log(`Direction: ${rover.direction}`);
-  
+  console.log(`Direction: ${marsRover.direction}`);
 }
 
-
-
-
-
-
-function checkEndOfMap(axis, moveDirectionIsNegative) {
+function checkEndOfMap(axis, moveDirectionIsNegative, marsRover) {
   if (moveDirectionIsNegative) {
-    if (rover[axis] - 1 < 0) {
+    if (marsRover[axis] - 1 < 0) {
       console.log('End of map.');
       return true;
     } 
 
   } else {
-    if (rover[axis] + 1 > 9) {
+    if (marsRover[axis] + 1 > 9) {
       console.log('End of map.');
       return true;
     } 
@@ -102,148 +125,194 @@ function checkIfMoveIsFree(nextMove) {
   if (nextMove === 'X') {
     console.log('Obstcal in the way');
     return false;
+  } else if (nextMove !== '_') {
+    console.log('Rover in the way');
+    return false;
+
   } else {
     return true;
   }
 }
 
-
-
-
-
-function moveForward(rover){
+function moveForward(marsRover){
   let nextMove;
   let axis;
   let moveDirectionIsNegative;
   console.log("moveForward was called");
-  map[rover.y][rover.x] = '_';
-  switch(rover.direction) {
+  map[marsRover.y][marsRover.x] = '_';
+  console.log(marsRover);
+  switch(marsRover.direction) {
     case 'N':
       //Checks if next move will be outside of map
       axis = 'y';
       moveDirectionIsNegative = true;
-      if (checkEndOfMap(axis, moveDirectionIsNegative)) break;
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
       //Checks if next move will colide with obstical
-      nextMove = map[rover.y - 1][rover.x];
+      nextMove = map[marsRover.y - 1][marsRover.x];
       if (checkIfMoveIsFree(nextMove)) {
-        rover.y--;
+        marsRover.y--;
       }
       break;
     case 'E':
     //Checks if next move will be outside of map
       axis = 'x';
       moveDirectionIsNegative = false;
-      if (checkEndOfMap(axis, moveDirectionIsNegative)) break;
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
       //Checks if next move will colide with obstical
-      nextMove = map[rover.y][rover.x + 1];
+      nextMove = map[marsRover.y][marsRover.x + 1];
       if (checkIfMoveIsFree(nextMove)) {
-        rover.x++;
+        marsRover.x++;
       }
       break;
     case 'S':
       //Checks if next move will be outside of map
       axis = 'y';
       moveDirectionIsNegative = false;
-      if (checkEndOfMap(axis, moveDirectionIsNegative)) break;
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
       //Checks if next move will colide with obstical
-      nextMove = map[rover.y + 1][rover.x];
+      nextMove = map[marsRover.y + 1][marsRover.x];
       if (checkIfMoveIsFree(nextMove)) {
-        rover.y++;
+        marsRover.y++;
       }
       break;
     case 'W':
       //Checks if next move will be outside of map
       moveDirectionIsNegative = true;
       axis = 'x';
-      if (checkEndOfMap(axis, moveDirectionIsNegative)) break;
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
       //Checks if next move will colide with obstical
-      nextMove = map[rover.y][rover.x - 1];
+      nextMove = map[marsRover.y][marsRover.x - 1];
       if (checkIfMoveIsFree(nextMove)) {
-        rover.x--;
+        marsRover.x--;
       }
       break;
     }
-    rover.travelLog.push({x: rover.x, y: rover.y});
-    map[rover.y][rover.x] = 'R';
-    console.log(`X:${rover.x}, Y:${rover.y}`);
-    console.log(map);
+    marsRover.travelLog.push({x: marsRover.x, y: marsRover.y});
+    map[marsRover.y][marsRover.x] = marsRover.letterOnMap;
     drawBoard();
 }
 
-function moveBackward(rover){
-  console.log("moveBackward was called")
-  
-  switch(rover.direction) {
+function moveBackward(marsRover){
+  let nextMove;
+  let axis;
+  let moveDirectionIsNegative;
+  console.log("moveBackward was called");
+  map[marsRover.y][marsRover.x] = '_';
+  console.log(marsRover);
+  switch(marsRover.direction) {
     case 'N':
-      rover.y++;
+      //Checks if next move will be outside of map
+      axis = 'y';
+      moveDirectionIsNegative = false;
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
+      //Checks if next move will colide with obstical
+      nextMove = map[marsRover.y + 1][marsRover.x];
+      if (checkIfMoveIsFree(nextMove)) {
+        marsRover.y++;
+      }
       break;
     case 'E':
-      rover--;
+    //Checks if next move will be outside of map
+      axis = 'x';
+      moveDirectionIsNegative = true;
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
+      //Checks if next move will colide with obstical
+      nextMove = map[marsRover.y][marsRover.x - 1];
+      if (checkIfMoveIsFree(nextMove)) {
+        marsRover.x--;
+      }
       break;
     case 'S':
-      rover.y--;
+      //Checks if next move will be outside of map
+      axis = 'y';
+      moveDirectionIsNegative = true;
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
+      //Checks if next move will colide with obstical
+      nextMove = map[marsRover.y - 1][marsRover.x];
+      if (checkIfMoveIsFree(nextMove)) {
+        marsRover.y--
+      }
       break;
     case 'W':
-      rover.x++;
+      //Checks if next move will be outside of map
+      moveDirectionIsNegative = false;
+      axis = 'x';
+      if (checkEndOfMap(axis, moveDirectionIsNegative, marsRover)) break;
+      //Checks if next move will colide with obstical
+      nextMove = map[marsRover.y][marsRover.x + 1];
+      if (checkIfMoveIsFree(nextMove)) {
+        marsRover.x++;
+      }
       break;
     }
-    checkMap();
-    console.log(`X:${rover.x}, Y:${rover.y}`);
-    rover.travelLog.push({x: rover.x, y: rover.y});
+    marsRover.travelLog.push({x: marsRover.x, y: marsRover.y});
+    map[marsRover.y][marsRover.x] = marsRover.letterOnMap
+    drawBoard();
 }
 
-function handleCommands(commands) {
+function handleCommands(commands, marsRover) {
   for (let i = 0; i < commands.length; i++) {
     switch(commands[i]) {
       case 'l':
-        turnLeft(rover);
+        turnLeft(marsRover);
         break;
       case 'r':
-        turnRight(rover);
+        turnRight(marsRover);
         break;
       case 'f':
-        moveForward(rover);
+        moveForward(marsRover);
         break;
       case 'b':
-        moveBackward(rover);
+        moveBackward(marsRover);
         break;
     }
   }
   console.log(rover.travelLog);
 }
 
-function validateCommand(command) {
+function validateCommand(command, marsRover) {
   var patt = /[^f|b|l|r]/i;
   if (!patt.test(command)) {
-      handleCommands(command);
+      handleCommands(command, marsRover);
   } else {
-      console.log('That is invalid command.')
+      console.log('That is invalid command.');
   }
 }
-
-
 
 document.addEventListener('keydown', (e) => {
   switch(e.keyCode) {
     case 37:
      //left
-     rover.direction = 'W';
-      validateCommand('f');
+      rover.direction = 'W';
+      validateCommand('f', rover);
       break;
     case 38:
       //up
       rover.direction = 'N';
-      validateCommand('f');
+      validateCommand('f', rover);
       break;
     case 39:
       //right
       rover.direction = 'E';
-      validateCommand('f');
+      validateCommand('f', rover);
       break;
     case 40:
       //down
       rover.direction = 'S';
-      validateCommand('f');
+      validateCommand('f', rover);
       break;
   }
 });
+
+function moveRoverClone(roverClone) {
+  setInterval(function(){
+    const randomNumbers = Math.floor(Math.random() * 3);
+    const commands = ['r', 'l', 'f', 'b'];
+    validateCommand(commands[randomNumbers], roverClone);
+   }, 250);
+
+}  
+
+moveRoverClone(roverCloneOne);
+moveRoverClone(roverCloneTwo);
+moveRoverClone(roverCloneThree);
--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -1,68 +1,37 @@
 const canvas = document.querySelector('#canvas');
-
-
-
-const boardSide = window.innerHeight * 0.8;
+const boardSide = window.innerHeight * 0.6;
 const squareSide = boardSide / 10;
+
 canvas.width = boardSide;
 canvas.height = boardSide;
 canvas.style.marginTop = (window.innerHeight * 0.1) + 'px';
 
-var ctx = canvas.getContext('2d');
-
-
-
-
+const ctx = canvas.getContext('2d');
 
 function drawBoard() {
-    const cols = 10;
-    const rows = 10;
-    for (let i = 0; i < cols; i++) {
+	const cols = 10;
+	const rows = 10;
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			let x = i * squareSide;
+			let y = j * squareSide;
+			cellColor = '#8C1818';
 
+			if (map[j][i] === 'X') cellColor = '#4C0D0D';
+	
+			if (map[j][i] === 'R') cellColor = '#7F7466';
 
-        
-        
+			if (map[j][i] === 'C') cellColor = '#C2B1A4';
 
-        for (let j = 0; j < rows; j++) {
-            let x = i * squareSide;
-            let y = j * squareSide;
-            
-            cellColor = '#8C1818';
-
-           
-
-            if (map[j][i] === 'X')  {
-
-                cellColor = '#4C0D0D';
-            }
-
-            if (map[j][i] === 'R')  {
-
-                cellColor = '#59524E';
-            }
-          
-
-
-
-
-            ctx.beginPath();
-            ctx.lineWidth = "4";
-            ctx.strokeStyle = "black";
-            ctx.fillStyle = cellColor;
-            ctx.fillRect(x, y, squareSide, squareSide);
-            ctx.rect(x, y, squareSide, squareSide);
-
-            ctx.stroke();
-
-
-
-
-        }
-       
-
-    }
-    
-
+			ctx.beginPath();
+			ctx.lineWidth = "4";
+			ctx.strokeStyle = "black";
+			ctx.fillStyle = cellColor;
+			ctx.fillRect(x, y, squareSide, squareSide);
+			ctx.rect(x, y, squareSide, squareSide);
+			ctx.stroke();
+		}
+	}
 }
 
 drawBoard(squareSide);
